5128db1d407c77e46909b74604dd1fc7a1f69b72
akakou <akakou571@gmail.com>
2022-04-06 22:14:05 +0900

    [add] test code of white list migration

--- model_test.go
+++ model_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"os"
 	"testing"
 )
 
@@ -14,6 +16,44 @@ func setupForTest() {
 	}
 
 	db = dbl
+}
+
+func TestMigrateWhiteList(t *testing.T) {
+	var fwls []string
+
+	setupForTest()
+
+	err := migrateWhiteList()
+
+	if err != nil {
+		t.Errorf("failed to migrate white list: %v", err)
+		return
+	}
+
+	b, _ := os.ReadFile(WHITELIST_FILE)
+	err = json.Unmarshal(b, &fwls)
+
+	if err != nil {
+		log.Fatalf("failed to load migrate.json: %v", err)
+	}
+
+	t.Run("test model", func(t *testing.T) {
+		dbwls, err := db.client.WhiteList.
+			Query().All(db.ctx)
+
+		if err != nil {
+			t.Errorf("failed read white list: %v", err)
+			return
+		}
+
+		for i := 0; i < len(dbwls); i++ {
+			if fwls[i] != dbwls[i].URL {
+				t.Errorf("not match migration configuration and db data: %v", err)
+
+			}
+		}
+	})
+
 }
 
 func TestModel(t *testing.T) {
