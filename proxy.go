6bf7432855239782f76db90002eec0372ffbd672
akakou <akakou571@gmail.com>
2022-06-14 02:10:32 +0900

    [add] interface of director and modify reponse

--- proxy.go
+++ proxy.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 )
 
-
+var directors []func(req *http.Request)
+var modifyResponses []func(req *http.Response)
 
 func director(req *http.Request) {
-
+	for _, d := range directors {
+		d(req)
+	}
 }
 
 func modifyResponse(res *http.Response) error {
+	for _, mR := range modifyResponses {
+		mR(res)
+	}
+
 	return nil
 }
