1e0e6c82e627cfa6d6f816fb2617a2b5577e8129
kino-ma <ma@kino.ma>
2022-07-02 00:34:50 +0900

    [add] XHR wrapper

--- wrapper/wrapper.js
+++ wrapper/wrapper.js
@@ -1,8 +1,17 @@
 const __originadFetch = window.fetch;
+const __originalXhr = window.XMLHttpRequest;
 
 function customFetch(...args) {
   console.log("fetch called");
   return __originadFetch(args);
 }
 
+class customXhr extends __originalXhr {
+  constructor() {
+    console.log("xhr created");
+    super();
+  }
+}
+
 window.fetch = customFetch;
+window.XMLHttpRequest = customXhr;
