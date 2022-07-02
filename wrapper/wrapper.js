30006756c03cfa916937bad9b3d760323c69177d
kino-ma <ma@kino.ma>
2022-07-02 12:06:29 +0900

    [add] normalize the fetch path

--- wrapper/wrapper.js
+++ wrapper/wrapper.js
@@ -1,9 +1,37 @@
+const PROXY_ADDR = "http://localhost:9000";
+
 const __originadFetch = window.fetch;
 const __originalXhr = window.XMLHttpRequest;
 
-function customFetch(...args) {
+function customFetch(input, init = {}) {
   console.log("fetch called");
-  return __originadFetch(args);
+
+  let newAddr = "";
+
+  // input is type of either string or Request
+  newAddr = typeof input === "string" ? input : input.url;
+
+  if (!newAddr.startsWith("http://") && !newAddr.startsWith("/")) {
+    // if it is relative path, first make it absolute
+
+    // hoge/fuga/piyo
+    //          ^ Get this index
+    const slashIndex = newAddr.lastIndexOf("/");
+    // hoge/fuga/piyo -> hoge/fuga/
+    location.pathname.slice(0, slashIndex + 1);
+  }
+
+  // if it is absolute path in the origin, convert it to URL
+  if (newAddr.startsWith("/")) {
+    newAddr = `${location.origin}${newAddr}`;
+  }
+
+  init.mode = "cors";
+
+  const url = PROXY_ADDR;
+
+  // TODO: maybe i need to add CORS-safelisted headers (https://fetch.spec.whatwg.org/#cors-safelisted-request-header)
+  return __originadFetch(url, init);
 }
 
 class customXhr extends __originalXhr {
