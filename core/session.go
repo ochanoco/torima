e385ae9d1611ef677fb9e871998ac1b5ed2221fe
akakou <akakou571@gmail.com>
2022-07-04 03:45:03 +0900

    [update] remmove token

--- core/session.go
+++ core/session.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net/http"
+)
+
+func RemoveToken(req *http.Request) {
+	queries := req.URL.Query()
+	queries.Del("token")
+
+	req.URL.RawQuery = queries.Encode()
+
+	cookies := req.Cookies()
+	cookies = delCookie("token", cookies)
+
+	for _, cookie := range cookies {
+		req.Header.Set("Set-Cookie", cookie.String())
+	}
+}
