9ed08c1a3dfa2154c76ee24ddf5c0bdd7c2233f0
akakou <akakou571@gmail.com>
2022-12-04 14:02:49 +0900

    [add] load html next.js  generated

--- api/main.go
+++ api/main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"gin_line_login"
+	"net/http"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
+const BASE_NEXT_PATH = "../app/out/"
+
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob(BASE_NEXT_PATH + "/*.html")
+	r.Static("/_next/", BASE_NEXT_PATH+"_next/")
 
 	store := cookie.NewStore([]byte("secret"))
 	r.Use(sessions.Sessions("mysession", store))
 
-	lineLogin, err := gin_line_login.NewLineLogin(r, "/auth/unauthorized", "/auth/callback", "/")
+	lineLogin, err := gin_line_login.NewLineLogin(r, "/login", "/auth/callback", "/redirect")
 	if err != nil {
 		panic(err)
 	}
@@ -32,8 +37,27 @@ func main() {
 		c.JSON(200, gin.H{"message": "logout"})
 	})
 
-	r.GET("/auth/unauthorized", func(c *gin.Context) {
-		c.JSON(401, gin.H{"message": "unauthorized"})
+	r.GET("/ochanoco/redirect", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "redirect.html", gin.H{})
 	})
+
+	r.GET("/ochanoco/callback", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "redirect"})
+	})
+
+	r.GET("/login", func(c *gin.Context) {
+		// todo: authenticate servicer
+		_, err := c.Get("client_id")
+		if err {
+		}
+
+		_, err = c.Get("client_secret")
+		if err {
+
+		}
+
+		c.HTML(http.StatusOK, "login.html", gin.H{})
+	})
+
 	r.Run()
 }
