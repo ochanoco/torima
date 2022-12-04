f58865765dabbb0c085d52c40f5bad4900ea05d8
akakou <akakou571@gmail.com>
2022-12-04 13:58:55 +0900

    [fix] path using as imported css

--- auth/app/pages/redirect.tsx
+++ auth/app/pages/redirect.tsx
@@ -2,7 +2,7 @@ import { TEXT_ON_REDIRET_PAGE } from '../languages/ja_jp'
 import { OCHANOCO_CONFIG } from '../config'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 import { useEffect } from 'react'
 
 
