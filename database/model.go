f288a8e06cd402b5bb47a71a6e6e3f5783afec72
akakou <akakou571@gmail.com>
2022-12-04 17:43:47 +0900

    [refactor] devide db to independ repository

--- database/model.go
+++ database/model.go
@@ -7,9 +7,17 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/ochanoco/ochano.co-auth/proxy/ent"
 )
+
+var DB_TYPE = "sqlite3"
+var DB_CONFIG = "file:./db.sqlite3?_fk=1"
+var WHITELIST_FILE = "./whitelist.json"
+
+var LOGIN_REDIRECT_PAGE_URL = "http://localhost:3000/redirect"
+var ERROR_PAGE_URL = "http://localhost:3000/error"
+
+var AUTH_PAGE_DOMAIN = "localhost:9000"
+var AUTH_PAGE_DESTINATION = "localhost:3000"
 
 type Database struct {
 	ctx    context.Context
