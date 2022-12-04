5904253907a676bdd3aa156fbd39d1e26c30c997
akakou <akakou571@gmail.com>
2022-12-04 18:34:56 +0900

    [update] public api to usable one

--- model_test.go
+++ model_test.go
@@ -5,17 +5,19 @@ import (
 	"log"
 	"os"
 	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func setupForTest() {
 	DB_CONFIG = "file:ent?mode=memory&cache=shared&_fk=1"
-	dbl, err := initDB()
+	dbl, err := InitDB()
 
 	if err != nil {
 		log.Panicf("failed init db: %v", err)
 	}
 
-	db = dbl
+	DB = dbl
 }
 
 func TestMigrateWhiteList(t *testing.T) {
@@ -23,7 +25,7 @@ func TestMigrateWhiteList(t *testing.T) {
 
 	setupForTest()
 
-	err := migrateWhiteList()
+	err := MigrateWhiteList()
 
 	if err != nil {
 		t.Errorf("failed to migrate white list: %v", err)
@@ -38,8 +40,8 @@ func TestMigrateWhiteList(t *testing.T) {
 	}
 
 	t.Run("test model", func(t *testing.T) {
-		dbwls, err := db.client.WhiteList.
-			Query().All(db.ctx)
+		dbwls, err := DB.client.WhiteList.
+			Query().All(DB.ctx)
 
 		if err != nil {
 			t.Errorf("failed read white list: %v", err)
@@ -47,7 +49,7 @@ func TestMigrateWhiteList(t *testing.T) {
 		}
 
 		for i := 0; i < len(dbwls); i++ {
-			if fwls[i] != dbwls[i].URL {
+			if fwls[i] != dbwls[i].Path {
 				t.Errorf("not match migration configuration and db data: %v", err)
 
 			}
@@ -60,20 +62,20 @@ func TestModel(t *testing.T) {
 	setupForTest()
 
 	t.Run("test model", func(t *testing.T) {
-		db, err := initDB()
+		db, err := InitDB()
 
 		if err != nil {
 			t.Errorf("model_test: %v", err)
 		}
 
-		wl := createWhiteList(db, "https://example.com")
+		wl := CreateWhiteList(db, "https://example.com")
 
 		if err != nil {
 			t.Errorf("failed creating white list: %v", err)
 			return
 		}
 
-		projc := createProject(db, "<domain>", "<destination>", "<line_id_for_model_test>", "<name>")
+		projc := CreateServiceProvider(db, "<domain>", "<destination>")
 		proj, nil := projc.Save(db.ctx)
 
 		if err != nil {
@@ -81,7 +83,7 @@ func TestModel(t *testing.T) {
 			return
 		}
 
-		proj, err = saveWhiteListOnProj(db, proj, wl)
+		proj, err = SaveWhiteListOnProj(db, proj, wl)
 
 		if err != nil {
 			t.Errorf("failed add white list to project: %v", err)
