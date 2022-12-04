d9c0c5b9fc9f5bb2084a1461252d03ee678c11e5
akakou <akakou571@gmail.com>
2022-12-04 11:21:42 +0900

    [update] remove legacy and  add simple page

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,62 +1,25 @@
+import { TEXT_IN_LOGIN_PAGE } from '../languages/ja_jp'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
-import { useSession, signIn, signOut, getSession } from "next-auth/react"
-import { generateToken } from '../lib/jwt'
 
 
-const LoginPage: NextPage = ({ token }) => {
-  const { data: session } = useSession()
-
-  if (session) {
-    console.log("data: ", token)
-
-    const refererUrl = localStorage.getItem('referer')
-    const redirectUrl = `${refererUrl}?token=${token}`
-
-
-    if (!refererUrl) return (
-      <div>
-        <div>Logged in</div>
-        <br />
-        <button onClick={() => signOut()}>Log out</button>
-      </div>
-    )
-
-    console.log("token: ", redirectUrl)
-    location.href = redirectUrl
-  }
-
+const LoginPage: NextPage = ({ }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Login Page</title>
+        <title>{TEXT_IN_LOGIN_PAGE.TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <button onClick={() => signIn()}>Log in</button>
+        <h1>{TEXT_IN_LOGIN_PAGE.HEADER}</h1>
+        <p>{TEXT_IN_LOGIN_PAGE.MESSAGE}</p >
+        <button onClick={() => {
+          location.href = '/auth/login'
+        }}>{TEXT_IN_LOGIN_PAGE.BUTTON}</button>
       </main>
     </div>
   )
-}
-
-
-export async function getServerSideProps(context) {
-  let token = ''
-  const session = await getSession(context)
-
-  if (session?.token) {
-    token = generateToken(session?.token.sub)
-  }
-
-  console.log(token)
-
-  return {
-    props: {
-      token
-    },
-  }
 }
 
 
