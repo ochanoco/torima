f6ae0fc3ae8fcd3714985612d3f533138d976c07
akakou <akakou571@gmail.com>
2022-12-04 13:55:10 +0900

    [add] front page using redirect

--- auth/app/pages/login.tsx
+++ auth/app/pages/login.tsx
@@ -1,4 +1,4 @@
-import { TEXT_IN_LOGIN_PAGE } from '../languages/ja_jp'
+import { TEXT_ON_LOGIN_PAGE } from '../languages/ja_jp'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
@@ -8,15 +8,15 @@ const LoginPage: NextPage = ({ }) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>{TEXT_IN_LOGIN_PAGE.TITLE}</title>
+        <title>{TEXT_ON_LOGIN_PAGE.TITLE}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>{TEXT_IN_LOGIN_PAGE.HEADER}</h1>
-        <p>{TEXT_IN_LOGIN_PAGE.MESSAGE}</p >
+        <h1>{TEXT_ON_LOGIN_PAGE.HEADER}</h1>
+        <p>{TEXT_ON_LOGIN_PAGE.MESSAGE}</p >
         <button onClick={() => {
           location.href = '/auth/login'
-        }}>{TEXT_IN_LOGIN_PAGE.BUTTON}</button>
+        }}>{TEXT_ON_LOGIN_PAGE.BUTTON}</button>
       </main>
     </div>
   )
