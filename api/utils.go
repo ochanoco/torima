cce9cd531e9f91b50281c518d696e0534a8300a3
akakou <akakou571@gmail.com>
2023-02-03 18:20:14 +0900

    [add] generate random string

--- api/utils.go
+++ api/utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/rand"
 	"net/http"
 )
 
@@ -18,4 +19,20 @@ func delCookie(name string, src []*http.Cookie) []*http.Cookie {
 	}
 
 	return dest
+}
+
+func randomString(length int) (string, error) {
+	const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	bytes := make([]byte, length)
+
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	for i, b := range bytes {
+		bytes[i] = chars[b%byte(len(chars))]
+	}
+
+	return string(bytes), nil
 }
