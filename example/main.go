e5a3ad82a6e62d9b33e46341eb5d985461d8c011
akakou <akakou571@gmail.com>
2023-04-07 02:05:26 +0900

    [rename] just rename ('api' => 'backend', 'auth' => 'authweb')

---
+++ example/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"testing"
+)
+
+func TestIntegration(t *testing.T) {
+	if os.Getenv("TEST_INTEGRATION") != "1" {
+		t.Skip("Skipping testing in All test")
+	}
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<a href='%v'>link</a>", "/ochanoco/redirect?callback_path=/hello")
+	})
+
+	server := httptest.NewServer(h)
+	defer server.Close()
+
+	db, err := InitDB(DB_CONFIG)
+	if err != nil {
+		log.Fatalf("failed to init db: %v", err)
+	}
+
+	servUrl, err := url.Parse(server.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sp := db.client.ServiceProvider.
+		Create().
+		SetHost("localhost:8080").
+		SetDestinationIP(servUrl.Host)
+
+	sp.SaveX(db.ctx)
+}
