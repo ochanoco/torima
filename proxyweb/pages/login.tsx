7358aa899d1eb178a0c53969e9cd0d00c2505d80
akakou <akakou571@gmail.com>
2023-04-07 08:32:34 +0900

    [move] path (/ochanoco/redirect => /ochanoco/login)

---
+++ proxyweb/pages/login.tsx
@@ -0,0 +1,30 @@
+import { TEXT_ON_REDIRET_PAGE } from '../languages/ja_jp'
+import { OCHANOCO_CONFIG } from '../config'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+
+
+const RedirectPage: NextPage = ({ }) => {
+    useEffect(() => {
+        setTimeout(() => {
+            location.href = "/ochanoco/redirect"
+        }, 3000)
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{TEXT_ON_REDIRET_PAGE.TITLE}</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <h1>{TEXT_ON_REDIRET_PAGE.HEADER}</h1>
+                <p>{TEXT_ON_REDIRET_PAGE.MESSAGE}</p>
+            </main>
+        </div>
+    )
+}
+
+export default RedirectPage
