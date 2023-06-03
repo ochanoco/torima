510e866088ef86e5c8f5d2a4df661db18464ef1b
kino-ma <ma@kino.ma>
2023-06-03 17:24:04 +0900

    feat: add dns library

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/miekg/dns"
+)
 
 func main() {
 	fmt.Println("hello, world")
+	c := new(dns.Client)
+	m := new(dns.Msg)
+	m.SetQuestion("google.com.", dns.TypeA)
+	m.RecursionDesired = true
+	r, _, err := c.Exchange(m, "8.8.8.8:53")
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, a := range r.Answer {
+		if a, ok := a.(*dns.A); ok {
+			fmt.Printf("%s\n", a.String())
+		}
+	}
 }
