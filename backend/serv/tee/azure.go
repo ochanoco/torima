1ef6f0f08a5366dfb7dd08509e92f18ce1304e69
akakou <akakou571@gmail.com>
2023-06-04 12:19:22 +0900

    [update] apply previous changes to some points

--- backend/serv/tee/azure.go
+++ backend/serv/tee/azure.go
@@ -1,42 +1,13 @@
 package tee
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"log"
-	"math/big"
-	"time"
 
 	"github.com/edgelesssys/ego/enclave"
-	"github.com/gin-gonic/gin"
 )
 
-func setupTLS() *tls.Config {
-	template := &x509.Certificate{
-		SerialNumber: &big.Int{},
-		Subject:      pkix.Name{CommonName: "localhost"},
-		NotAfter:     time.Now().Add(time.Hour),
-		DNSNames:     []string{"localhost"},
-	}
-
-	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
-	cert, _ := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
-
-	tlsCfg := tls.Config{
-		Certificates: []tls.Certificate{
-			{
-				Certificate: [][]byte{cert},
-				PrivateKey:  priv,
-			},
-		},
-	}
-	return &tlsCfg
-}
-
-func setupAttestaion(r *gin.Engine, tlsConfig *tls.Config) {
+func setupAttestaion(tlsConfig *tls.Config) string {
 	token, err := enclave.CreateAzureAttestationToken(tlsConfig.Certificates[0].Certificate[0], attestationProviderURL)
 	if err != nil {
 		log.Print("Run without attestation!!!!!\n")
@@ -44,7 +15,5 @@ func setupAttestaion(r *gin.Engine, tlsConfig *tls.Config) {
 		log.Print("Created an Microsoft Azure Attestation Token.")
 	}
 
-	r.Any("/token", func(c *gin.Context) {
-		c.String(200, token)
-	})
+	return token
 }
