61fa52dd63bb002886faa507b945f744c4e68b2d
akakou <akakou571@gmail.com>
2023-06-04 11:10:03 +0900

    [add] utili function for hash chain

--- backend/core/utils.go
+++ backend/core/utils.go
@@ -2,11 +2,14 @@ package core
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/ochanoco/proxy/ent"
 )
 
 func RandomString(length int) (string, error) {
@@ -42,4 +45,24 @@ func ReadHTTPBody(body *io.ReadCloser) ([]byte, error) {
 	*body = b
 
 	return bodyBuf, err
+}
+
+type HashChain = ent.HashChain
+
+func CalcHashChain(log *ent.ServiceLog, previous *ent.HashChain) []byte {
+	var previous_buf []byte
+
+	if previous_buf == nil {
+		previous_buf = []byte("invalid")
+	} else {
+		previous_buf = previous.Hash
+	}
+
+	hasher := crypto.SHA256.New()
+	hasher.Write([]byte(log.Headers))
+	hasher.Write(log.Body)
+	hasher.Write(previous_buf)
+	hash := hasher.Sum(nil)
+
+	return hash
 }
