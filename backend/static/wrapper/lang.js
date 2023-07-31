52883fe5e471380d5b080895c68b2a887b766e10
akakou <akakou571@gmail.com>
2023-07-31 09:46:19 +0900

    feat: stop to use proxy and use popup

---
+++ backend/static/wrapper/lang.js
@@ -0,0 +1,15 @@
+const CONTENTS_TEXT = {
+    'EN': {
+        'title': 'TODO',
+        'content': `
+        TODO: Add content here.`,
+    },
+    'JA': {
+        'title': '最初のポップアップ',
+        'content': `
+        このサイトでは、セキュリティのためにLINEログインをお願いしています。
+        あと5秒でログインサイトにジャンプします。
+
+        詳細は https://ochano.co をご覧ください。`,
+    },
+}
