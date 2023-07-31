9e8bca083e98e6e6cb8105be743c5a644fd89fbd
akakou <akakou571@gmail.com>
2023-07-31 10:12:06 +0900

    refactor: remove unused files

--- proxyweb/languages/ja_jp.ts
+++
@@ -1,9 +0,0 @@
-export const SITE_NAME = "ochano.co 不正アクセス防止機構"
-
-export const TEXT_ON_REDIRET_PAGE: any = {
-    TITLE: `リダイレクトしています || ${SITE_NAME}`,
-    HEADER: '不正アクセス対策サイトにリダイレクトしています',
-    MESSAGE: [
-        "お客様がご利用されたWebサイトでは、不正アクセス防止のためリダイレクトしています。",
-        "リダイレクトは3秒後に行われます。しばらくお待ちください。"],
-}
--- proxyweb/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/ochanoco',
-}
-
-module.exports = nextConfig
