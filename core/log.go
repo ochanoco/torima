c6480ed9edd9385b0fd9919c9cb7e1f02ccb08ac
akakou <akakou571@gmail.com>
2023-08-05 11:12:05 +0900

    fix: divide log to header and body

--- core/log.go
+++ core/log.go
@@ -1,14 +1,22 @@
 package core
 
 import (
+	"bytes"
 	"time"
 
 	"github.com/ochanoco/proxy/ent"
 )
 
-func logRawCommunication(_type string, header string, body []byte, proxy *OchanocoProxy) (*ent.CommunicationLog, error) {
+func logRawCommunication(_type string, message []byte, proxy *OchanocoProxy) (*ent.CommunicationLog, error) {
 	time := time.Now()
 
-	l := proxy.Database.CommunicationLog(_type, time, header, body)
+	splited := bytes.Split(message, []byte("\r\n\r\n"))
+
+	header := splited[0]
+	headerLen := len(header)
+
+	body := message[headerLen:]
+
+	l := proxy.Database.CommunicationLog(_type, time, string(header), body)
 	return proxy.Database.SaveCommunicateLog(l)
 }
