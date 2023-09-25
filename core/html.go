4e2bf8e3f6051b4c25932aefd6fd6b77c9ec15e5
akakou <akakou571@gmail.com>
2023-09-26 03:39:40 +0900

    feat: html page

---
+++ core/html.go
@@ -0,0 +1,12 @@
+package core
+
+var scripts = `
+<script src="https://cdn.jsdelivr.net/npm/@simondmc/popup-js@1.4.2/popup.min.js"></script>
+<script src="/ochanoco/static/wrapper/lang.js"></script>
+<script src="/ochanoco/static/wrapper/popup.js"></script>
+<script src="/ochanoco/static/wrapper/register_sw.js"></script>
+`
+
+var forceOpenPopup = `
+<script src="/ochanoco/static/wrapper/open.js"></script>
+`
